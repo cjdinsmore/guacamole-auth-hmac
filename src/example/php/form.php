dc1f7e6ca677deaf71df5d2a7c12720986b8b3a1
Calvin Mclean <calvinmclean@email.arizona.edu>
2016-09-27 15:26:34 -0700

    php small changes

--- src/example/php/form.php
+++ src/example/php/form.php
@@ -1,5 +1,5 @@
 <?php
-$username = 'calvinmclean';
+$username = 'username';
 $hostname = 'hostname';
 $protocol = 'vnc';
 $port = 5901;
@@ -11,32 +11,26 @@ $vncPass = 'password';
 $secretKey = 'secret';
 
 $timestamp = time() * 1000;
-
-function signParams($timestamp, $protocol, $secretKey) {
-  return base64_encode( hash_hmac( 'sha256', $timestamp . $protocol, $secretKey, 1 ) );
-}
 ?>
 
 <!DOCTYPE html>
+<meta charset='UTF-8'>
 <html>
   <head>
     <title>Guacamole Button</title>
   </head>
   <body>
-    <form method="post" action="http://localhost:8888/guacamole/#/client" class="inline">
+    <form enctype='application/x-www-form-urlencoded' method='POST' action='http://localhost:8888/guacamole/#/client/<?= $id; ?>'>
 
-      <input type="hidden" name="id" value="<?php echo $id; ?>">
-      <input type="hidden" name="timestamp" value="<?php echo $timestamp; ?>">
-      <!-- <input type="hidden" name="guac.username" value="<?php echo $username; ?>"> -->
-      <input type="hidden" name="guac.port" value="<?php echo $port; ?>">
-      <input type="hidden" name="guac.hostname" value="<?php echo $hostname; ?>">
-      <input type="hidden" name="guac.protocol" value="<?php echo $protocol; ?>">
-      <input type="hidden" name="guac.password" value="<?php echo $vncPass; ?>">
-      <input type="hidden" name="signature" value="<?php echo signParams($timestamp, $protocol, $secretKey); ?>">
+      <input type='hidden' name='id' value='<?= urlencode('c/'.$id) ?>'>
+      <input type='hidden' name='timestamp' value='<?= urlencode($timestamp) ?>'>
+      <input type='hidden' name='guac.port' value='<?= urlencode($port) ?>'>
+      <input type='hidden' name='guac.hostname' value='<?= urlencode($hostname) ?>'>
+      <input type='hidden' name='guac.protocol' value='<?= urlencode($protocol) ?>'>
+      <input type='hidden' name='guac.password' value='<?= urlencode($vncPass) ?>'>
+      <input type='hidden' name='signature' value='<?php echo base64_encode( hash_hmac( 'sha256', $timestamp . $protocol, $secretKey, 1 ) ); ?>'>
 
-      <button type="submit" name="submit_param" value="submit_value" class="link-button">
-        CONNECT
-      </button>
+      <input type='submit' value='click for Guacamole' >
 
     </form>
   </body>
